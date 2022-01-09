cc8fd3f13d820c16b7bdbffa0a5a2cea48bd2a29
Jiyoung Hwang <jiyounghwang92@gmail.com>
2022-01-09 19:30:34 +0900

    ✨ Add loginPage initial js

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// 링크 클릭 시 페이지 전환
+function nextPage1() {
+  location.href = "./loginEmail.html";
+}
+
+function nextPage2() {
+  location.href = "./joinMembership.html";
+}
---
+++ js/splashScreen.js
@@ -0,0 +1,6 @@
+// 스플래시 페이지에서 로그인 페이지로 넘어가는 화면
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    location.href = "./login.html";
+  }, 1000);
+})
