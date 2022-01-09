0ea3f45460a0d08203d1653031db0fc7c3e83f88
ywhkr <dnsghk2@gmail.com>
2022-01-09 23:20:20 +0900

    :white_check_mark: comment-btn fix

--- js/postpage.js
+++ js/postpage.js
@@ -1,4 +1,4 @@
-const commentBtn = document.querySelector('.comment-btn');
+const commentBtn = document.querySelectorAll('.comment-btn');
 const postBtn = document.querySelector('.post-btn');
 const postInput = document.querySelector('.post-input');
 const postModal = document.querySelector('.post-modal');
@@ -6,24 +6,26 @@ const reportBtn = document.querySelector('.post-modal__content__report-btn');
 const cancleBtn = document.querySelector('.modal-confirm__button--no');
 const newModal = document.querySelector('.modal-window');
 
-postInput.addEventListener('keydown', (e) => {
+postInput.addEventListener('keydown', () => {
   postBtn.classList.add('on');
 });
 
-postBtn.addEventListener('click', (e) => {
+postBtn.addEventListener('click', () => {
   postInput.value = '';
   postBtn.classList.remove('on');
 });
 
-commentBtn.addEventListener('click', (e) => {
-  postModal.classList.toggle('show');
-});
+commentBtn.forEach((el) =>
+  el.addEventListener('click', () => {
+    postModal.classList.toggle('show');
+  })
+);
 
-reportBtn.addEventListener('click', (e) => {
+reportBtn.addEventListener('click', () => {
   newModal.classList.add('show');
   postModal.classList.remove('show');
 });
 
-cancleBtn.addEventListener('click', (e) => {
+cancleBtn.addEventListener('click', () => {
   newModal.classList.remove('show');
 });
