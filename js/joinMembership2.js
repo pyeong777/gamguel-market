ee31bb932ca8ab36336cead57b83a4aec6a7c8bd
Jiyoung Hwang <jiyounghwang92@gmail.com>
2022-01-12 18:53:55 +0900

    💄 Fix joinMembership2Page html, css, js

--- js/joinMembership2.js
+++ js/joinMembership2.js
@@ -1,8 +1,8 @@
 // name, id, introduce 입력 시 버튼 활성화
-const nameInput = document.getElementById('name'),
-  idInput = document.getElementById('id'),
-  introduceInput = document.getElementById('introduce');
-const startBtn = document.getElementById('start_btn');
+let nameInput = document.getElementById('name');
+let idInput = document.getElementById('join_id');
+let introduceInput = document.getElementById('introduce');
+let startBtn = document.getElementById('start_btn');
 
 const isActiveStart = () => {
   let nameValue = nameInput.value;
@@ -41,11 +41,43 @@ function nextPage() {
 
 
 // 유효성 검사
-$(function () {
-  if ("조건1") {
-    document.getElementsByClassName("warning").style.display = "none";
-  } else {
-    document.getElementsByClassName("warning").style.display = "block";
+// * 이미 사용중인 아이디입니다.
+idInput.onblur = function () {
+  if (
+    (idInput.value == "deepdive123")
+  ) {
+    idInput.classList.add('invalid');
+    error.innerHTML = '*이미 사용중인 아이디입니다.';
   }
-})
+};
 
+idInput.onfocus = function () {
+  if (this.classList.contains('invalid')) {
+    this.classList.remove('invalid');
+    error.innerHTML = "";
+  }
+};
+
+
+// *영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.
+idInput.onblur = function () {
+  let regExp = /[A-Za-z0-9]/;
+
+  if (
+    !(
+      (regExp.test(idInput.value)) ||
+      (idInput.value.includes('_')) ||
+      (idInput.value.includes('.'))
+    )
+  ) {
+    idInput.classList.add('invalid');
+    error2.innerHTML = '*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.';
+  }
+};
+
+idInput.onfocus = function () {
+  if (this.classList.contains('invalid')) {
+    this.classList.remove('invalid');
+    error2.innerHTML = "";
+  }
+};
