36249d4b09581658373b23b8f1e2e5a722348b44
Tesseract <tensai3355@gmail.com>
2022-01-16 03:52:43 +0900

    :recycle: Refactor code and add thumbnail function

--- js/join.js
+++ js/join.js
@@ -2,14 +2,19 @@
 
 const initActive = (handler, elems) => {
   elems.forEach(elem => {
-    elem.addEventListener('input', handler);
-    elem.addEventListener('keyup', handler);
+    ['input', 'keyup']
+      .forEach(event => elem.addEventListener(event, handler));
   });
 };
 
 const isValidEmail = (email) => {
   const emailValidation = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
   return emailValidation.test(email);
+};
+
+const isValidId = (id) => {
+  const idValidation = /[^A-Za-z0-9._]/;
+  return !idValidation.test(id);
 };
 
 const showMsg = (elem, msg, isError = true) => {
@@ -34,8 +39,10 @@ const hideMsg = (elem) => {
 const idInput = document.getElementById('login_id');
 const pwInput = document.getElementById('login_pw');
 const nextBtn = document.getElementById('next_btn');
+const error1 = document.getElementById('error1');
+const error2 = document.getElementById('error2');
 
-const isActiveJoin = () => {
+const activateJoin = () => {
   const idValue = idInput.value;
   const pwValue = pwInput.value;
   
@@ -48,14 +55,16 @@ const isActiveJoin = () => {
     nextBtn.disabled = false;
     nextBtn.style.opacity = 1;
     nextBtn.style.cursor = 'pointer';
+    nextBtn.addEventListener('click', gotoNext);
   } else {
     nextBtn.disabled = true;
     nextBtn.style.opacity = .3;
+    nextBtn.style.cursor = 'default';
+    nextBtn.removeEventListener('click', gotoNext);
   }
 };
 
-const validateEmail = ({ currentTarget }) => {
-  const { value } = currentTarget;
+const validateEmail = ({ currentTarget: { value } }) => {
   NAME_SPACE.isUniqueEmail = false;
   if (isValidEmail(value)) {
     fetch(`${API}/user/emailvalid`, reqData('POST', {
@@ -71,6 +80,7 @@ const validateEmail = ({ currentTarget }) => {
         } else {
           showMsg(error1, '사용 가능한 이메일입니다.', false);
           NAME_SPACE.isUniqueEmail = true;
+          activateJoin();
         }
       });
   } else {
@@ -78,10 +88,14 @@ const validateEmail = ({ currentTarget }) => {
   }
 };
 
-const validatePassword = ({ currentTarget }) => {
-  const { value } = currentTarget;
+const validatePassword = ({ currentTarget: { value } }) => {
   if (value.length >= 6) return;
   showMsg(error2, '비밀번호는 6자 이상이어야 합니다.');
+};
+
+const gotoNext = () => {
+  document.querySelector('.join-form').style.display = "none";
+  document.querySelector('.profile-form').style.display = "block";
 };
 
 // 이메일 유효성 검사
@@ -92,14 +106,8 @@ idInput.addEventListener('focus', () => hideMsg(error1));
 pwInput.addEventListener('blur', validatePassword);
 pwInput.addEventListener('focus', () => hideMsg(error2));
 
-// 다음 버튼
-nextBtn.addEventListener('click', () => {
-  document.querySelector('.join-membership-container').style.display = "none";
-  document.querySelector('.join-membership2-container').style.display = "block";
-});
-
 // 모든 input이 유효하면 다음 버튼 활성화
-initActive(isActiveJoin, [idInput, pwInput]);
+initActive(activateJoin, [idInput, pwInput]);
 
 
 // join 2 - 프로필 설정
@@ -109,104 +117,121 @@ const joinIdInput = document.getElementById('join_id');
 const introInput = document.getElementById('introduce');
 const startBtn = document.getElementById('start_btn');
 const fileInput = document.getElementById('file');
+const profileImage = document.querySelector('.profile-img');
+const error3 = document.getElementById('error3');
+const error4 = document.getElementById('error4');
 
-const isActiveStart = () => {
+const activateStart = () => {
   const nameValue = nameInput.value;
   const joinIdValue = joinIdInput.value;
   const introValue = introInput.value;
 
   if (
-    (nameValue && joinIdValue && introValue) &&
-    // 아래는 이미 회원가입 되어 있는 정보라고 가정
-    !(joinIdValue == "deepdive123")
+    nameValue.length >= 2 &&
+    isValidId(joinIdValue) &&
+    NAME_SPACE.isUniqueId &&
+    introValue
   ) {
     startBtn.disabled = false;
     startBtn.style.opacity = 1;
     startBtn.style.cursor = 'pointer';
+    startBtn.addEventListener('click', joinEmail);
   }
   else {
     startBtn.disabled = true;
     startBtn.style.opacity = .3;
+    startBtn.style.cursor = 'default';
+    startBtn.removeEventListener('click', joinEmail);
   }
-}
+};
 
-initActive(isActiveStart, [nameInput, joinIdInput, introInput]);
+const setThumbnail = ({ currentTarget: { files } }) => {
+  if (!files.length) return;
+  const reader = new FileReader();
+  reader.onload = e => {
+    profileImage.src = e.target.result;
+  };
+  reader.readAsDataURL(files[0]);
+};
 
+const validateName = ({ currentTarget: { value } }) => {
+  if (value.length >= 2) return;
+  showMsg(error3, '이름은 2자 이상이어야 합니다.');
+};
 
-// 유효성 검사
-joinIdInput.addEventListener('focus', function () {
-  if (this.classList.contains('invalid3')) {
-    this.classList.remove('invalid3');
-    error3.innerHTML = "";
-  }
-});
-
-// *영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.
-joinIdInput.addEventListener('blur', function () {
-  let regExp = /[^A-Za-z0-9._]/;
-
-  if (
-    regExp.test(joinIdInput.value)
-  ) {
-    joinIdInput.classList.add('invalid4');
-    error4.innerHTML = '*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.';
-  } else {
+const validateId = ({ currentTarget: { value } }) => {
+  NAME_SPACE.isUniqueId = false;
+  if (isValidId(value)) {
+    if (!value.length) return;
     fetch(`${API}/user`, reqData())
       .then(res => res.json())
       .then(json => {
-        if (json.find(({ accountname }) => accountname === joinIdInput.value)) {
-          joinIdInput.classList.add('invalid4');
-          error4.innerHTML = '*이미 사용 중인 ID입니다.';
+        if (json.find(({ accountname }) => accountname === value)) {
+          showMsg(error4, '이미 사용 중인 ID입니다.');
         } else {
-          joinIdInput.classList.remove('invalid4');
-          error4.innerHTML = '';
+          showMsg(error4, '사용 가능한 ID입니다.', false);
+          NAME_SPACE.isUniqueId = true;
+          activateStart();
         }
       });
+  } else {
+    showMsg(error4, '영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.');
   }
-});
+};
 
-joinIdInput.addEventListener('focus', function () {
-  if (this.classList.contains('invalid4')) {
-    this.classList.remove('invalid4');
-    error4.innerHTML = "";
-  }
-});
+const joinEmail = async () => {
+  const formData = new FormData();
+  formData.append('image', fileInput.files[0]);
+  let res = await fetch(`${API}/image/uploadfile`, {
+    method: 'POST',
+    body: formData
+  });
+  let json = await res.json();
+  
+  const { filename } = json;
 
-
-
-
-
-// API 연동
-function joinEmail() {
-  fetch(`${API}/user`, reqData('POST', {
+  res = await fetch(`${API}/user`, reqData('POST', {
     user: {
       email: idInput.value,
       password: pwInput.value,
       username: nameInput.value,
       accountname: joinIdInput.value,
       intro: introInput.value,
-      image: fileInput.value
+      image: `${API}/${filename}`
     }
-  }))
-    .then(res => res.json())
-    .then(({ user }) => {
-      if (!user) return;
-      fetch(`${API}/user/login`, reqData('POST', {
-        user: {
-          email: idInput.value,
-          password: pwInput.value
-        }
-      }))
-        .then(res => res.json())
-        .then(({ user }) => {
-          const { _id, accountname, token } = user;
-          localStorage.setItem("token", token);
-          localStorage.setItem("userid", _id);
-          localStorage.setItem("accountname", accountname);
-          location.href = "../index.html";
-        });
-    });
-}
+  }));
+  json = await res.json();
 
-document.querySelector('#start_btn').addEventListener('click', joinEmail);
+  let { user } = json;
+  if (!user) return;
+
+  res = await fetch(`${API}/user/login`, reqData('POST', {
+    user: {
+      email: idInput.value,
+      password: pwInput.value
+    }
+  }));
+  json = await res.json();
+
+  const { _id, accountname, token } = json.user;
+  localStorage.setItem("userid", _id);
+  localStorage.setItem("accountname", accountname);
+  localStorage.setItem("token", token);
+  location.href = "../index.html";
+};
+
+// 이미지 썸네일
+fileInput.addEventListener('change', setThumbnail);
+
+// 이름 유효성 검사
+nameInput.addEventListener('blur', validateName);
+nameInput.addEventListener('focus', () => hideMsg(error3));
+
+// 아이디 유효성 검사
+joinIdInput.addEventListener('blur', validateId);
+joinIdInput.addEventListener('focus', () => hideMsg(error4));
+
+// 모든 input이 유효하면 시작 버튼 활성화
+initActive(activateStart, [nameInput, joinIdInput, introInput]);
+
 const NAME_SPACE = getNameSpace();
