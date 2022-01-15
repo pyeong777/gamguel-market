78ff71574377a5385351508b22a6f4f9ef08af2e
Tesseract <tensai3355@gmail.com>
2022-01-16 00:36:09 +0900

    :recycle: Refactor functions related to email and password

--- js/join.js
+++ js/join.js
@@ -1,101 +1,109 @@
+// 유틸 함수들
+
+const initActive = (handler, elems) => {
+  elems.forEach(elem => {
+    elem.addEventListener('input', handler);
+    elem.addEventListener('keyup', handler);
+  });
+};
+
+const isValidEmail = (email) => {
+  const emailValidation = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  return emailValidation.test(email);
+};
+
+const showMsg = (elem, msg, isError = true) => {
+  if (isError) {
+    elem.textContent = '*' + msg;
+  } else {
+    elem.textContent = msg;
+    elem.style.color = 'rgb(52, 188, 97)';
+  }
+};
+
+const hideMsg = (elem) => {
+  if (elem.textContent !== '') {
+    elem.textContent = '';
+    elem.style.color = 'rgb(255, 0, 0)';
+  }
+};
+
+
 // join 1 - 이메일로 회원가입
-//id, pw 입력 시 버튼 활성화
+
 const idInput = document.getElementById('login_id');
 const pwInput = document.getElementById('login_pw');
 const nextBtn = document.getElementById('next_btn');
 
 const isActiveJoin = () => {
-  let idValue = idInput.value;
-  let pwValue = pwInput.value;
-
+  const idValue = idInput.value;
+  const pwValue = pwInput.value;
+  
   if (
-    (idValue && pwValue) &&
-    (pwValue.length >= 6) &&
-    (idValue.includes('@')) &&
-    // 아래는 이미 회원가입 되어 있는 정보라고 가정
-    !(idValue == "deepdive@naver.com")
-    // !(pwValue == "123456")
+    idValue &&
+    pwValue.length >= 6 &&
+    isValidEmail(idValue) &&
+    NAME_SPACE.isUniqueEmail
   ) {
     nextBtn.disabled = false;
     nextBtn.style.opacity = 1;
     nextBtn.style.cursor = 'pointer';
-  }
-  else {
+  } else {
     nextBtn.disabled = true;
     nextBtn.style.opacity = .3;
   }
-}
+};
 
-const initJoin = () => {
-  idInput.addEventListener('input', isActiveJoin);
-  pwInput.addEventListener('input', isActiveJoin);
-  idInput.addEventListener('keyup', isActiveJoin);
-  pwInput.addEventListener('keyup', isActiveJoin);
-}
-
-initJoin();
-
-
-// 버튼 클릭 시 
-nextBtn.addEventListener('click', () => {
-  document.querySelector('.join-membership-container').style.display = "none";
-  document.querySelector('.join-membership2-container').style.display = "block";
-});
-
-
-// 유효성 검사 (addEventListener로 수정)
-// id
-idInput.addEventListener('blur', () => {
-  const emailValidation = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-  if (emailValidation.test(idInput.value)) {
-    fetch('http://146.56.183.55:5050/user/emailvalid', reqData('POST', {
+const validateEmail = ({ currentTarget }) => {
+  const { value } = currentTarget;
+  NAME_SPACE.isUniqueEmail = false;
+  if (isValidEmail(value)) {
+    fetch(`${API}/user/emailvalid`, reqData('POST', {
       user: {
-        email: idInput.value
+        email: value
       }
     }))
       .then(res => res.json())
       .then(json => {
         const { message } = json;
-        if (message === '이미 가입된 이메일 주소 입니다.') error1.textContent = message;
+        if (message === '이미 가입된 이메일 주소 입니다.') {
+          showMsg(error1, message);
+        } else {
+          showMsg(error1, '사용 가능한 이메일입니다.', false);
+          NAME_SPACE.isUniqueEmail = true;
+        }
       });
   } else {
-    error1.textContent = '올바른 이메일 형식이 아닙니다.'
+    showMsg(error1, '올바른 이메일 형식이 아닙니다.');
   }
+};
 
+const validatePassword = ({ currentTarget }) => {
+  const { value } = currentTarget;
+  if (value.length >= 6) return;
+  showMsg(error2, '비밀번호는 6자 이상이어야 합니다.');
+};
+
+// 이메일 유효성 검사
+idInput.addEventListener('blur', validateEmail);
+idInput.addEventListener('focus', () => hideMsg(error1));
+
+// 비밀번호 유효성 검사
+pwInput.addEventListener('blur', validatePassword);
+pwInput.addEventListener('focus', () => hideMsg(error2));
+
+// 다음 버튼
+nextBtn.addEventListener('click', () => {
+  document.querySelector('.join-membership-container').style.display = "none";
+  document.querySelector('.join-membership2-container').style.display = "block";
 });
 
-idInput.addEventListener('focus', function () {
-  if (this.classList.contains('invalid1')) {
-    this.classList.remove('invalid1');
-    error1.innerHTML = "";
-  }
-});
-
-
-// pw
-pwInput.addEventListener('blur', function () {
-  if (
-    (pwInput.value.length < 6)
-  ) {
-    pwInput.classList.add('invalid2');
-    error2.innerHTML = '*비밀번호는 6자 이상이어야 합니다.'
-  }
-});
-
-
-pwInput.addEventListener('focus', function () {
-  if (this.classList.contains('invalid2')) {
-    this.classList.remove('invalid2');
-    error2.innerHTML = "";
-  }
-});
-
-
-
+// 모든 input이 유효하면 다음 버튼 활성화
+initActive(isActiveJoin, [idInput, pwInput]);
 
 
 // join 2 - 프로필 설정
-// name, id, introduce 입력 시 버튼 활성화
+
 const nameInput = document.getElementById('name');
 const joinIdInput = document.getElementById('join_id');
 const introInput = document.getElementById('introduce');
@@ -103,9 +111,9 @@ const startBtn = document.getElementById('start_btn');
 const fileInput = document.getElementById('file');
 
 const isActiveStart = () => {
-  let nameValue = nameInput.value;
-  let joinIdValue = joinIdInput.value;
-  let introValue = introInput.value;
+  const nameValue = nameInput.value;
+  const joinIdValue = joinIdInput.value;
+  const introValue = introInput.value;
 
   if (
     (nameValue && joinIdValue && introValue) &&
@@ -122,16 +130,7 @@ const isActiveStart = () => {
   }
 }
 
-const initStart = () => {
-  nameInput.addEventListener('input', isActiveStart);
-  joinIdInput.addEventListener('input', isActiveStart);
-  introInput.addEventListener('input', isActiveStart);
-  nameInput.addEventListener('keyup', isActiveStart);
-  joinIdInput.addEventListener('keyup', isActiveStart);
-  introInput.addEventListener('keyup', isActiveStart);
-}
-
-initStart();
+initActive(isActiveStart, [nameInput, joinIdInput, introInput]);
 
 
 // 유효성 검사
@@ -152,7 +151,7 @@ joinIdInput.addEventListener('blur', function () {
     joinIdInput.classList.add('invalid4');
     error4.innerHTML = '*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.';
   } else {
-    fetch(`http://146.56.183.55:5050/user`, reqData())
+    fetch(`${API}/user`, reqData())
       .then(res => res.json())
       .then(json => {
         if (json.find(({ accountname }) => accountname === joinIdInput.value)) {
@@ -179,7 +178,7 @@ joinIdInput.addEventListener('focus', function () {
 
 // API 연동
 function joinEmail() {
-  fetch('http://146.56.183.55:5050/user', reqData('POST', {
+  fetch(`${API}/user`, reqData('POST', {
     user: {
       email: idInput.value,
       password: pwInput.value,
@@ -192,7 +191,7 @@ function joinEmail() {
     .then(res => res.json())
     .then(({ user }) => {
       if (!user) return;
-      fetch('http://146.56.183.55:5050/user/login', reqData('POST', {
+      fetch(`${API}/user/login`, reqData('POST', {
         user: {
           email: idInput.value,
           password: pwInput.value
