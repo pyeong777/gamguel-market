e0e1bb2d29c15b8512874d0dd19a8c570030bfdf
Tesseract <tensai3355@gmail.com>
2022-01-16 22:25:05 +0900

    :recycle: Use query string instead of localStorage

--- js/constant.js
+++ js/constant.js
@@ -1,1 +1,5 @@
 const API = 'http://146.56.183.55:5050';
+const MINUTE = 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const MONTH = DAY * 30;
--- js/postpage.js
+++ js/postpage.js
@@ -2,7 +2,6 @@
 const backButton = document.querySelector('.main-header__back');
 const menuButton = document.querySelector('.main-header__button');
 
-// 모달
 // 모달 - setting
 const modal = document.querySelector('.modal');
 const modalWindows = document.querySelectorAll('.modal-window');
@@ -20,19 +19,13 @@ const postButtons = postModal.querySelectorAll('.modal-bottom__button');
 const myPostModal = document.querySelector('.modal-bottom__post--my-account');
 const deletePostModal = document.querySelector('.modal-delete-posting');
 const myPostButtons = myPostModal.querySelectorAll('.modal-bottom__button');
-const deletePostButtons = deletePostModal.querySelectorAll(
-  '.modal-confirm__button'
-);
+const deletePostButtons = deletePostModal.querySelectorAll('.modal-confirm__button');
 
 // 모달 - myComment
-const commentModal = document.querySelector(
-  '.modal-bottom__comment--my-account'
-);
+const commentModal = document.querySelector('.modal-bottom__comment--my-account');
 const commentButton = commentModal.querySelector('.modal-bottom__button');
 const deleteCommentModal = document.querySelector('.modal-delete-comment');
-const deleteCommentButtons = deleteCommentModal.querySelectorAll(
-  '.modal-confirm__button'
-);
+const deleteCommentButtons = deleteCommentModal.querySelectorAll('.modal-confirm__button');
 
 // 댓글
 const postBtn = document.querySelector('.post-btn');
@@ -46,7 +39,7 @@ const logout = () => {
 
 const showModal = (type, ...args) => {
   modal.classList.add('is-modal-active');
-  const [id, link] = args;
+  const [id] = args;
   switch (type) {
     case 'setting':
       settingModal.classList.add('is-modal-active');
@@ -56,17 +49,6 @@ const showModal = (type, ...args) => {
       settingModal.classList.remove('is-modal-active');
       logoutModal.classList.add('is-modal-active');
       break;
-    case 'product':
-      productModal.classList.add('is-modal-active');
-      deleteProductModal.classList.remove('is-modal-active');
-      if (!args.length) return;
-      localStorage.setItem('productId', id);
-      localStorage.setItem('productLink', link);
-      break;
-    case 'deleteProduct':
-      productModal.classList.remove('is-modal-active');
-      deleteProductModal.classList.add('is-modal-active');
-      break;
     case 'post':
       postModal.classList.add('is-modal-active');
       break;
@@ -74,7 +56,7 @@ const showModal = (type, ...args) => {
       myPostModal.classList.add('is-modal-active');
       deletePostModal.classList.remove('is-modal-active');
       if (!args.length) return;
-      localStorage.setItem('postId', id);
+      NAME_SPACE.postId = id;
       break;
     case 'deletePost':
       myPostModal.classList.remove('is-modal-active');
@@ -83,7 +65,8 @@ const showModal = (type, ...args) => {
     case 'comment':
       commentModal.classList.add('is-modal-active');
       deleteCommentModal.classList.remove('is-modal-active');
-      localStorage.setItem('commentId', id);
+      if (!args.length) return;
+      NAME_SPACE.commentId = id;
       break;
     case 'deleteComment':
       commentModal.classList.remove('is-modal-active');
@@ -98,15 +81,7 @@ const hideModal = (e) => {
 };
 
 const fetchFeed = () => {
-  const id = localStorage.getItem('postId');
-  const token = localStorage.getItem('token');
-  fetch(`${API}/post/${id}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
+  fetch(`${API}/post/${NAME_SPACE.postId}`, reqData())
     .then((res) => res.json())
     .then((json) => renderFeed(json));
 };
@@ -173,9 +148,8 @@ const renderFeed = (json) => {
     ?.addEventListener('mousewheel', horizontalScroll);
   item.querySelector('.btn-heart').addEventListener('click', toggleHeart);
 
-  const selectedUser = localStorage.getItem('selectedUser');
   const accountname = localStorage.getItem('accountname');
-  if (selectedUser === accountname) {
+  if (NAME_SPACE.user === accountname) {
     item
       .querySelector('.feed-article__button')
       .addEventListener('click', () => showModal('myPost', id));
@@ -187,37 +161,14 @@ const renderFeed = (json) => {
 };
 
 const deletePost = () => {
-  const id = localStorage.getItem('postId');
-  const token = localStorage.getItem('token');
-  fetch(`${API}/post/${id}`, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  }).then(() => {
-    gotoPage('main.html');
-  });
-};
-
-const horizontalScroll = (e) => {
-  const { wheelDelta, currentTarget } = e;
-  const { offsetWidth, scrollLeft, scrollWidth } = currentTarget;
-  if (offsetWidth + scrollLeft >= scrollWidth && wheelDelta < 0) return;
-  if (scrollLeft === 0 && wheelDelta > 0) return;
-  e.preventDefault();
-  currentTarget.scrollLeft -= wheelDelta / 2;
+  fetch(`${API}/post/${NAME_SPACE.postId}`, reqData('DELETE'))
+    .then(() => {
+      gotoPage('main.html');
+    });
 };
 
 const fetchOneFeed = (id, elem) => {
-  const token = localStorage.getItem('token');
-  fetch(`${API}/post/${id}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
+  fetch(`${API}/post/${id}`, reqData())
     .then((res) => res.json())
     .then(({ post }) => {
       const { hearted, heartCount } = post;
@@ -233,59 +184,21 @@ const fetchOneFeed = (id, elem) => {
 
 const toggleHeart = ({ currentTarget }) => {
   const { hearted, id } = currentTarget.dataset;
-  const token = localStorage.getItem('token');
   if (+hearted) {
-    fetch(`${API}/post/${id}/unheart`, {
-      method: 'DELETE',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    }).then(() => {
-      fetchOneFeed(id, currentTarget);
-    });
+    fetch(`${API}/post/${id}/unheart`, reqData('DELETE'))
+      .then(() => {
+        fetchOneFeed(id, currentTarget);
+      });
   } else {
-    fetch(`${API}/post/${id}/heart`, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    }).then(() => {
-      fetchOneFeed(id, currentTarget);
-    });
+    fetch(`${API}/post/${id}/heart`, reqData('POST'))
+      .then(() => {
+        fetchOneFeed(id, currentTarget);
+      });
   }
 };
 
-const getAlbumItem = ({ image }, index) => {
-  const images = image.split(',').filter((img) => img !== '');
-  if (!images.length) return;
-  let multiHTML = '';
-  if (images.length > 1) multiHTML = 'feed-album__item--multi';
-  const item = document.createElement('li');
-  item.innerHTML = `
-  <button type="button" class="feed-album__button">
-    <img src="${images[0]}" alt="피드 이미지"
-    onerror="this.src='../images/full-logo.svg'" class="feed-album__picture">
-  </button>
-  `;
-  item.setAttribute('class', `feed-album__item ${multiHTML}`);
-  item.setAttribute('style', `order:${index}`);
-  return item;
-};
-
-// 댓글 기능 혼자 구현해보기 (에러)
-
 const fetchComment = () => {
-  const id = localStorage.getItem('postId');
-  const token = localStorage.getItem('token');
-  fetch(`${API}/post/${id}/comments`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
+  fetch(`${API}/post/${NAME_SPACE.postId}/comments`, reqData())
     .then((res) => res.json())
     .then((json) => renderComment(json));
 };
@@ -315,14 +228,14 @@ const renderComment = (json) => {
     fragment.appendChild(item);
     item
       .querySelector('.user-img')
-      .addEventListener('click', () =>
-        gotoPage('profile.html', { selectedUser: author.accountname })
-      );
+      .addEventListener('click', () => {
+        gotoPage('profile.html', { user: author.accountname }, [ 'user' ]);
+      });
     item
       .querySelector('.user-name')
-      .addEventListener('click', () =>
-        gotoPage('profile.html', { selectedUser: author.accountname })
-      );
+      .addEventListener('click', () => {
+        gotoPage('profile.html', { user: author.accountname }, [ 'user' ]);
+      });
     const accountname = localStorage.getItem('accountname');
     if (author.accountname === accountname) {
       item
@@ -339,66 +252,44 @@ const renderComment = (json) => {
 
 const fetchCommentImage = () => {
   const accountname = localStorage.getItem('accountname');
-  const token = localStorage.getItem('token');
-  fetch(`${API}/profile/${accountname}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
+  fetch(`${API}/profile/${accountname}`, reqData())
     .then((res) => res.json())
     .then(({ profile }) => (postProfile.src = profile.image));
 };
 
 const deleteComment = () => {
-  const postId = localStorage.getItem('postId');
-  const id = localStorage.getItem('commentId');
-  const token = localStorage.getItem('token');
-  fetch(`${API}/post/${postId}/comments/${id}`, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  }).then(() => {
-    fetchComment();
-    modal.classList.remove('is-modal-active');
-    modalWindows.forEach((modal) => modal.classList.remove('is-modal-active'));
-  });
+  const { postId, commentId } = NAME_SPACE;
+  fetch(`${API}/post/${postId}/comments/${commentId}`, reqData('DELETE'))
+    .then(() => {
+      fetchComment();
+      modal.classList.remove('is-modal-active');
+      modalWindows.forEach((modal) => modal.classList.remove('is-modal-active'));
+    });
 };
 
 const getElapsedTime = (time) => {
   const ms = Date.parse(time);
   const now = Date.now();
   const diff = (now - ms) / 1000;
-  if (diff < 60) return `${parseInt(diff)}초`;
-  else if (diff < 3600) return `${parseInt(diff / 60)}분`;
-  else if (diff < 86400) return `${parseInt(diff / 3600)}시간`;
-  else if (diff < 2592000) return `${parseInt(diff / 86400)}일`;
-  else return `${parseInt(diff / 2592000)}달`;
+  if (diff < MINUTE) return `${parseInt(diff)}초`;
+  else if (diff < HOUR) return `${parseInt(diff / MINUTE)}분`;
+  else if (diff < DAY) return `${parseInt(diff / HOUR)}시간`;
+  else if (diff < MONTH) return `${parseInt(diff / DAY)}일`;
+  else return `${parseInt(diff / MONTH)}달`;
 };
 
 const addComment = () => {
   const { value } = postInput;
   if (value === '') return;
-  const postId = localStorage.getItem('postId');
-  const token = localStorage.getItem('token');
-  fetch(`${API}/post/${postId}/comments`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
+  fetch(`${API}/post/${NAME_SPACE.postId}/comments`, reqData('POST', {
+    comment: {
+      content: value,
     },
-    body: JSON.stringify({
-      comment: {
-        content: value,
-      },
-    }),
-  }).then(() => {
-    postInput.value = '';
-    fetchComment();
-  });
+  }))
+    .then(() => {
+      postInput.value = '';
+      fetchComment();
+    });
 };
 
 // 헤더 버튼
@@ -417,9 +308,9 @@ logoutButtons[1].addEventListener('click', logout);
 // 내 피드 게시물 메뉴 누를 때 뜨는 모달 버튼
 postButtons[0].addEventListener('click', hideModal);
 myPostButtons[0].addEventListener('click', () => showModal('deletePost'));
-myPostButtons[1].addEventListener('click', () =>
-  gotoPage('upload.html', { page: 'modifyPost' })
-);
+myPostButtons[1].addEventListener('click', () => {
+  gotoPage('upload.html', { page: 'modifyPost' }, [ 'page', 'postId' ]);
+});
 deletePostButtons[0].addEventListener('click', () => showModal('myPost'));
 deletePostButtons[1].addEventListener('click', deletePost);
 
@@ -436,6 +327,7 @@ postInput.addEventListener('keydown', () => {
 
 postBtn.addEventListener('click', addComment);
 
+const NAME_SPACE = getNameSpace();
 fetchFeed();
 fetchComment();
 fetchCommentImage();
