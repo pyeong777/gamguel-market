1397bc427b6ec50f0fe72c3d436c0301e051e94f
DaePyeongSeo <sdp1123@naver.com>
2022-01-17 21:05:26 +0900

    🐛 Fix index heart style

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@ const fetchOneFeed = (id, elem) => {
       const { hearted, heartCount } = post;
       elem.innerHTML = `
       <img src="../images/icon-heart${hearted ? '-active' : ''}.svg" alt="post-like" class="article-heart__btn">
-      <span class="article-num">${heartCount}</span>
+      <span class="article-num article-heart__num">${heartCount}</span>
       `;
       elem.setAttribute('data-hearted', hearted ? 1 : 0);
 })
@@ -173,11 +173,11 @@ async function getFeed() {
                     ${imageHTML}
                     <button type="button" data-hearted="${hearted ? 1 : 0}" data-id="${id}" class="btn-heart">
                     <img src="../images/icon-heart${hearted ? '-active' : ''}.svg" alt="post-like" class="article-heart__btn">
-                    <span class="article__num article-heart__num">${heartCount}</span>
+                    <span class="article-num article-heart__num">${heartCount}</span>
                     </button>
                     <button type="button" class="btn-comment">
                         <img src="../images/icon-comment.svg" alt="post-comment" class="article-comment__btn">
-                        <span class="article__num article-comment__num">${commentCount}</span>
+                        <span class="article-num article-comment__num">${commentCount}</span>
                     </button>
                     <strong class="article-date">${year}년 ${month}월 ${day}일</strong>
                 </div>
